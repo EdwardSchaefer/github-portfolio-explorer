2b51752fd9c53d9156a073b364e2155240c74768
Edward Schaefer <18139334+EdwardSchaefer@users.noreply.github.com>
2019-01-07 21:06:51 -0500

    Add input to set username and store in localstorage and lead username on load in AppComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'gpe-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'github-portfolio-explorer';
+  public username: string;
+  public repos: any;
+  constructor() {
+    this.username = localStorage.getItem('username');
+  }
+  setUsername(e) {
+    if (e.keyCode === 13) {
+      e.target.blur();
+      localStorage.setItem('username', e.target.value);
+    }
+  }
 }
