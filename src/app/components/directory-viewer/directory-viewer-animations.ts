e1d551bf16676e9ffc52a900bf0602a98f71499f
Edward Schaefer <18139334+EdwardSchaefer@users.noreply.github.com>
2019-02-07 20:11:52 -0500

    Convert to MatTree from nested to flat. Fix a bug where animations would not register on sub folders when closing a node because of nested structure.

--- src/app/components/directory-viewer/directory-viewer-animations.ts
+++ src/app/components/directory-viewer/directory-viewer-animations.ts
@@ -3,22 +3,16 @@ import {animate, style, transition, trigger, sequence, query} from '@angular/ani
 export const DirectoryViewerAnimations = [
   trigger('directoryViewer', [
     transition(':enter', [
-      query(':enter, .gpe-folder-button',
-        sequence([
-          style({ 'minHeight': '0px', height: '0px' }),
-          animate('0.25s ease-out', style({ height: '*'})),
-        ]),
-        {optional: true}
-      )
+      sequence([
+        style({ 'minHeight': '0px', height: '0px' }),
+        animate('0.25s ease-out', style({ 'minHeight': '*', height: '*'})),
+      ])
     ]),
     transition(':leave', [
-      query(':leave, gpe-folder-button',
-        sequence([
-          style({ 'minHeight': '*', height: '*' }),
-          animate('0.25s ease-in', style({ 'minHeight': '0', height: '0px'})),
-        ]),
-        {optional: true}
-      )
+      sequence([
+        style({ 'minHeight': '*', height: '*' }),
+        animate('0.25s ease-in', style({ 'minHeight': '0', height: '0'})),
+      ])
     ])
   ])
 ];
--- src/app/components/directory-viewer/directory-viewer.component.ts
+++ src/app/components/directory-viewer/directory-viewer.component.ts
@@ -1,8 +1,19 @@
 import {Component, OnChanges, OnInit} from '@angular/core';
-import {MatTreeNestedDataSource} from '@angular/material/tree';
-import {NestedTreeControl} from '@angular/cdk/tree';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {FlatTreeControl} from '@angular/cdk/tree';
 import {DataService} from '../../data.service';
 import {DirectoryViewerAnimations} from './directory-viewer-animations';
+
+interface FlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
+interface TreeNode {
+  name: string;
+  children?: TreeNode[];
+}
 
 @Component({
   selector: 'gpe-directory-viewer',
@@ -11,34 +22,41 @@ import {DirectoryViewerAnimations} from './directory-viewer-animations';
   animations: [DirectoryViewerAnimations]
 })
 export class DirectoryViewerComponent implements OnInit, OnChanges {
-  nestedTreeControl: NestedTreeControl<any>;
-  nestedDataSource: MatTreeNestedDataSource<any>;
+  private transformer = (node: TreeNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node['path'],
+      level: level,
+    };
+  }
+  treeControl = new FlatTreeControl<FlatNode>(node => node.level, node => node.expandable);
+  treeFlattener = new MatTreeFlattener(this.transformer, node => node.level,
+      node => node.expandable, node => node.children);
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
   tree: any;
   constructor(public data: DataService) {
-    this.nestedTreeControl = new NestedTreeControl<any>(this._getChildren);
-    this.nestedDataSource = new MatTreeNestedDataSource();
     data.constructedTree.subscribe(selectedData => {
-      this.nestedDataSource.data = selectedData;
+      this.dataSource.data = selectedData;
     });
   }
   ngOnInit() {}
   ngOnChanges() {
     this.tree = this.data.repos[this.data.selectedIndex];
-    this.nestedDataSource.data = this.tree;
+    this.dataSource.data = this.tree;
   }
   endNode(node) {
-    this.data.getFile(node['path']);
+    this.data.getFile(node['name']);
   }
   getFileName(node) {
-    return node.path.split('/')[node['path'].split('/').length - 1];
+    return node.name.split('/')[node['name'].split('/').length - 1];
   }
   selected(node) {
-    if (node['path'] === this.data.selectedPath) {
+    if (node['name'] === this.data.selectedPath) {
       return 'gpe-file-list-item-selected';
     } else {
       return 'gpe-file-list-item';
     }
   }
-  hasNestedChild = (_: number, nodeData: any) => nodeData.type === 'tree';
+  hasChild = (_: number, node: FlatNode) => node.expandable;
   private _getChildren = (node) => node.children;
 }
