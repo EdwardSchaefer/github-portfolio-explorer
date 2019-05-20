153bf394057244bd422a5aa5b5d4c03dbcc54e02
Edward Schaefer <18139334+EdwardSchaefer@users.noreply.github.com>
2019-05-20 19:12:54 -0400

    Refactor authInterceptor

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,30 +1,22 @@
 import { Injectable } from '@angular/core';
 import {HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token: string = localStorage.getItem('token');
+    let headers = new HttpHeaders({
+      'Authorization': 'token ' + token
+    });
     if (req['url'].slice(req['url'].length - 7, req['url'].length) === '/readme') {
-      const headers = new HttpHeaders({
-        'Authorization': 'token ' + token,
-        'Accept': 'application/vnd.github.v3.html'
-      });
-      const authReq = req.clone({
-        headers: headers
-      });
-      return next.handle(authReq);
-    } else if (req['url'].slice(8, 11) === 'api') {
-      const headers = new HttpHeaders({
-        'Authorization': 'token ' + token
-      });
-      const authReq = req.clone({
-        headers: headers
-      });
-      return next.handle(authReq);
+      headers = headers.append('Accept', 'application/vnd.github.v3.html');
     }
-    return next.handle(req);
+    const authReq = req.clone({
+      headers: headers
+    });
+    return next.handle(authReq);
   }
 }
